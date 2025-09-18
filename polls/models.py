601fc88022ba48ec002178f0df8365d66c6e86eb
Glory-Jacintha <gloryjacintha.d@gmail.com>
2025-09-18 19:33:19 +0530

    object implementation

--- polls/models.py
+++ polls/models.py
@@ -1,10 +1,24 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
+
 
 class Question(models.Model):
     question_text = models.CharField(max_length = 200)
     publish_date = models.DateTimeField("Published Date")
 
+    def __str__(self):
+        return self.question.text
+    
+    def recently_published(self):
+        return self.publish_date >= timezone.now() - datetime.timedelta(days=1)
+
+
 class choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length = 200)
     votes = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return self.choice_text
