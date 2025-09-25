795c2607a18572ad65b044e11e91bc0f596b7d49
Glory-Jacintha <gloryjacintha.d@gmail.com>
2025-09-25 19:11:46 +0530

    Form processing

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404, HttpResponse
-from .models import Question
+from django.http import HttpResponseRedirect, HttpResponse
+from django.db.models import F
+from django.urls import reverse
+
+from .models import Question, Choice
 
 def index(request):
     latest_question_list = Question.objects.order_by("-publish_date")[:5]
@@ -17,4 +20,20 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk = question_id)
+    try:
+        selected_choice = question.choice_set.get(pk = request.POST["choice"])
+    except (KeyError,  Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You don't select any choice!",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args = (question.id)))
+    
