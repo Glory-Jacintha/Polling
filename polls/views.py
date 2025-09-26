27fb2b5016501a7d5c735765a5acf2aca71bcf81
Glory-Jacintha <gloryjacintha.d@gmail.com>
2025-09-26 18:47:58 +0530

    votes

--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,27 @@ from django.db.models import F
 from django.urls import reverse
 
 from .models import Question, Choice
+from django.views import generic
+
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-publish_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
 
 def index(request):
     latest_question_list = Question.objects.order_by("-publish_date")[:5]
